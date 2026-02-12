a7f7af6d641f801cbee6987aeb3968f7c9f3f128
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-12 15:52:43 +0200

    base routes are made

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
+# we use dotenv to load environment variables from a .env file
+# this should be done before importing any other modules that might use those environment variables
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
 app = FastAPI()
-
-@app.get("/welcome")
-def Welcome():
-    return "Welcome to FastAPI!"
+# this for telling the app to use the routes defined in base.py
+app.include_router(base.base_router)
