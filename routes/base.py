b2c2978ba7ccaec51ba4562c504efb2459fa2318
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-12 15:52:43 +0200

    base routes are made

---
+++ routes/base.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, APIRouter
+# we use os to access environment variables
+import os
+
+base_router = APIRouter(
+# this is the prefix for all routes defined in this router, so all routes will start with /api/v1
+    prefix="/api/v1",
+)
+@base_router.get("/")
+def Welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    return f"Welcome to FastAPI{app_name},{app_version}!"
