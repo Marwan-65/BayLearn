a8d7b0baa17eb39cd27ef6af933318e1a2fb8352
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-12 21:43:35 +0200

    - removed load env and made settings a struct that can be used to load env vars into it. - used depends to make sure that settings are loaded before the server starts. - restored main file as it was deleted by mistake. - made helpers folder and made config file to load env vars into settings struct.

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+"""
+we use dotenv to load environment variables from a .env file
+this should be done before importing any other modules that might use those environment variables
+from dotenv import load_dotenv
+load_dotenv(".env")
+"""
+from routes import base
+app = FastAPI()
+# this for telling the app to use the routes defined in base.py
+app.include_router(base.base_router)
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, APIRouter
-# we use os to access environment variables
-import os
+from fastapi import FastAPI, APIRouter, Depends
+from helpers.config import get_settings,Settings
 
 base_router = APIRouter(
 # this is the prefix for all routes defined in this router, so all routes will start with /api/v1
     prefix="/api/v1",
 )
 @base_router.get("/")
-def Welcome():
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
+def Welcome(app_settings: Settings = Depends(get_settings)):
+    # app_settings = get_settings()
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return f"Welcome to FastAPI {app_name},{app_version}!"
