12b435895439a217cc3d957e60e307197fd68cb1
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-13 02:02:28 +0200

    upload file feature

--- src/controllers/data.py
+++ src/controllers/data.py
@@ -6,7 +6,6 @@ class DataController(BaseController):
     def __init_(self):
         super().__init__()
         self.size_scale = 1048576  # 1 MB in bytes
-        
     def validate_file(self,file:UploadFile):
         # Implement file validation logic here
         if (self.app_settings.FILE_MAX_SIZE * self.size_scale) <= file.size:
