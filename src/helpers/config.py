aa42da56648b8a98974080f744d81d133af4c5bd
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-13 20:46:06 +0200

    -file uploaded succeesfully - file uploaded as chunks - aiofiles downloaded - response model error solved

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,10 +1,11 @@
-from pydantic_settings import BaseSettings 
+from pydantic_settings import BaseSettings, SettingsConfigDict 
 # define data specs to some validation on it
 class Settings(BaseSettings):
     APP_NAME:str
     APP_VERSION:str
     FILE_ALLOWED_EXTENSIONS:list
     FILE_MAX_SIZE:int
+    FILE_CHUNK_SIZE:int
 # here we load the .env file to get the environment variables defined in it
     class Config:
         env_file = ".env"
