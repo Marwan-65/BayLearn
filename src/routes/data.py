e205a0e41ea6b8b29f701e3257cfa65af952eaad
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-13 16:10:17 +0200

    making instansce methods

--- src/routes/data.py
+++ src/routes/data.py
@@ -9,11 +9,11 @@ data_router = APIRouter(
 @data_router.get("/upload/{project_id}")
 async def upload_file(project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)):
 # validate the file properties but as it is logic will be in controllers directory
-    is_valid,result_signal = DataController.validate_file(file=file)
+    is_valid,result_signal = DataController().validate_file(file=file)
     if not is_valid:
         return JSONResponse(
             status_code= status.HTTP_400_BAD_REQUEST,
             content={"message": result_signal}
         )
-    return ProjectController.make_project_dir(project_id=project_id)
+    return ProjectController().make_project_dir(project_id=project_id)
         
