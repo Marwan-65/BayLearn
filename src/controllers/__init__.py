2a43dd89d731a08465afc9140c36dbcd53cc969b
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-13 02:02:28 +0200

    upload file feature

---
+++ src/controllers/__init__.py
@@ -0,0 +1,2 @@
+# This file is used as a mirror for all general needs of the controller directory
+from .data import DataController
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter,FastAPI,Depends,UploadFile
 from helpers.config import get_settings,Settings
+from controllers import data
 
 data_router = APIRouter(
     prefix="/api/v1",
 )
 @data_router.get("/upload/{project_id}")
-def get_data(project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)):
-   # validate the file properties but as it is logic will be in controllers directory
-    return 
+async def upload_file(project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)):
+# validate the file properties but as it is logic will be in controllers directory
+    is_valid,result_signal = data.validate_file(file=file)
+    return {
+        "is_valid": is_valid,
+        "result_signal": result_signal
+    }
