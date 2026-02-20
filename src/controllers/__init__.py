d47d0cf23a88d5055004d9cf4591d9ceda892ce1
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-21 01:45:43 +0200

    `Added VectorDBProviderFactory and updated BaseController`

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,3 +1,4 @@
 # This file is used as a mirror for all general needs of the controller directory
 from .data import DataController
 from .project import ProjectController
+from .base import BaseController
