1b938762b869da55f051a283b529171366acba61
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-02-21 01:45:43 +0200

    `Added VectorDBProviderFactory and updated BaseController`

--- src/controllers/base.py
+++ src/controllers/base.py
@@ -9,4 +9,11 @@ class BaseController:
         self.project_dir_path = os.path.join(self.base_dir_path,"assets/files") # join adapts to the os and creates the path for the project directory
     def generate_random_string(self,length:int=12):
         return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-        
+    def get_database_path(self, db_name: str):
+        database_path = os.path.join(
+            self.database_dir, db_name
+        )
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+        return database_path
+            
