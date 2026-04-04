c3ec182669f1e99bf4be7a48422273ca79f6c31a
Rehab Ahmed <rehabfecky@gmail.com>
2026-04-04 11:52:27 +0200

    hanwdwritten notes extraction using gemini api + embedded images handling using paddleOCR

--- Input-Parsing-Module/app/main.py
+++ Input-Parsing-Module/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from .controllers.upload_controller import router
+from .config import load_environment
+
+load_environment()
 
 app = FastAPI(title="Input Parsing Module")
 
