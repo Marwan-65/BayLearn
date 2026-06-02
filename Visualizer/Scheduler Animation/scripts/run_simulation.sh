f939de73c5c47f15857c94e5d189ebb38bf4055f
Marwan-65 <marwan.emam.20@gmail.com>
2026-06-02 18:16:33 +0300

    Scheduler clarification

--- Visualizer/Scheduler Animation/scripts/run_simulation.sh
+++ Visualizer/Scheduler Animation/scripts/run_simulation.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
+
+#safety flags for bash
+#-e exit if any command fails
+# -u treat unset variables as errors and exit
+# -o pipefail if any command in a pipeline (cmd1 | cmd2) fails, the whole pipeline fails (not just the last command)
 set -euo pipefail
 
+#$n is the nth passed variable
+#:- value means use that value as default if argument not passed
 SCH="${1:-2}"
 Q="${2:-3}"
 INPUT="${3:-processes.txt}"
 
 cd /workspace
 
-make build
+make build #produce the process_generator.out binary
+# Stop any existing scheduler container to ensure a clean state (ignore errors if it doesn't exist)
 make stop || true
 
 cd scheduler
