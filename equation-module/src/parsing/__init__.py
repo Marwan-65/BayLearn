28c0ebbc2bf2d5339cb7e43b9467524ad675d453
Salma-nasser <133383515+Salma-nasser@users.noreply.github.com>
2026-06-05 17:41:34 +0300

    merged main and fixed wrong equation

--- equation-module/src/parsing/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Parsing exports."""
-
-from .llm_parser import translate_natural_language_to_math_json
-from .schema import EquationPayload, SolverPayload
-from .validators import validate_math_request_structure
-
-__all__ = ["EquationPayload", "SolverPayload", "translate_natural_language_to_math_json", "validate_math_request_structure"]
